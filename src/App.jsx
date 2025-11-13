7294629809c5838f821e5df6c99009d2a718f3ed
Eva Yonin <eva.yonin@gmail.com>
2025-11-13 14:09:14 +0200

    תיאור השינוי

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 // #2
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import ClientsData from './ClientsData'
 
